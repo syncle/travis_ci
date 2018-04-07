8ede4eff476689038b0266a04bab440c5fbf995e
Jaesik Park <syncle@gmail.com>
2018-04-07 03:22:26 -0700

    adding package

--- util/scripts/make-documentation.sh
+++ util/scripts/make-documentation.sh
@@ -2,9 +2,9 @@
 
 # open3d.org/docs
 pip install sphinx sphinx-autobuild sphinx-rtd-theme
-cd ../../docs && make html && cd ../util/scripts
+# cd ../../docs && make html && cd ../util/scripts
 
 # open3d.org/cppapi
 sudo apt-get -y install doxygen
-cd ../../
-doxygen Doxyfile
+# cd ../../
+# doxygen Doxyfile
